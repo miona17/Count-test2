521bc8c26c14c344488f49277dcbc6401e770f47
倉茂未央那 <miona17@icloud.com>
2017-11-25 16:21:52 +0900

    test

--- Count test2/ViewController.swift
+++ Count test2/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,65 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else{
+            label.textColor = UIColor.black
+        }
+        
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else{
+            label.textColor = UIColor.black
+        }
+    }
+    
+        @IBAction func times(){
+        number = number * 2
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else{
+            label.textColor = UIColor.black
+            
+        }
+    }
+    
+    @IBAction func devide(){
+        number = number / 2
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else{
+            label.textColor = UIColor.black
+        }
+        if number % 3 == 0{
+            label.text = "ma"
+            
+        }
+    }
+    
+    @IBAction func clear(){
+        number = number * 0
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else{
+            label.textColor = UIColor.black
+            
+        }
+    }
 }
+
+
+
 
